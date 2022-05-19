c2b60bd6918da974114181eebdcd43c32a32558c
Phoebus Petsimeris <79011574+Phoebus@users.noreply.github.com>
2022-05-19 23:08:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def get_movie_id(movie_name):
   movie_id = json_data['results'][0]['id']
   return(movie_id)
 
-#Uses the if returned by the get_movie_id function and gets information regarding ratings of the movie
+#Uses the id returned by the get_movie_id function and gets information regarding ratings of the movie
 #The three ratings that are stored are from IMDB, Metacritic and RottenTomatoes
 def get_movie_rating(movie_id):
   response = requests.get('https://imdb-api.com/en/API/Ratings/' + os.environ['IMDB_KEY'] + '/' + movie_id)
@@ -40,7 +40,7 @@ async def on_message(message):
     movie_ratings = get_movie_rating(movie_id)
     await message.channel.send("IMDB: " + movie_ratings[0] + ", Metacritic: " + movie_ratings[1] + ", RottenTomatoes: " + movie_ratings[2])
     
-#Keep alive is a different file that is required so that I can host the Bot. After that I use Uptime Robot to keep pinging it so that it always stays on
+#Keep alive is a different file that is required so that I can keep my Bot alive, with the use of Uptime Robot
 #even when the repl.it tabs are closed. I currently don't possess specific information about it, I just use it to keep my Bot alive at all times. I will
 #upload it to GitHub in case some wants to use it as well.
 keep_alive()
